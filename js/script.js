b1fdce51a3041f69d62a0da80172335449bbd8d6
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-25 15:23:48 +0600

    C_product section_C

--- js/script.js
+++ js/script.js
@@ -47,3 +47,29 @@ window.onscroll = ()=> {
     navbar.classList.remove('active');
 
 }
+
+/** swipper */
+var swiper = new Swiper(".product-slider", {
+
+    loop: true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+   
+    breakpoints: {
+     0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      
+      },
+    },
+  });
